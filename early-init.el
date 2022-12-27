88aa34c9be8f5876d474ddc6542f8bb6e662827f
Masayuki Hatta <mhatta@mhatta.org>
2022-12-28 01:44:02 +0900

    Updated

--- early-init.el
+++ early-init.el
@@ -11,8 +11,8 @@
 ;;;
 ;;; Profiler (start)
 ;;;
-(require 'profiler)
-(profiler-start 'cpu)
+;;(require 'profiler)
+;;(profiler-start 'cpu)
 
 ;; Prevent package.el loading packages prior to their init-file loading (for straight.el)
 (setq package-enable-at-startup nil)
